0997217062cd5a81c1738a4b623806131b79d124
Aflal <aftocr@gmail.com>
2024-04-24 09:43:45 +0530

    Update validate_tag.sh

--- scripts/validate_tag.sh
+++ scripts/validate_tag.sh
@@ -51,6 +51,8 @@ echo "COMMIT_BRANCH4: $(git describe --contains $CI_COMMIT_TAG)"
 echo "COMMIT_BRANCH4: $(git branch --contains $(git rev-list -n 1 $CI_COMMIT_TAG))"
 echo "DESIGNATED_BRANCHcheck: $(git branch --contains "$CI_COMMIT_SHA" | grep -q "$DESIGNATED_BRANCH")"
 echo "RELEASE_BRANCHcheck: $(git branch --contains "$CI_COMMIT_SHA" | grep -q "$RELEASE_BRANCH")"
+echo "git branch --contains "$CI_COMMIT_SHA" | grep -q "$RELEASE_BRANCH": $(git branch --contains "$CI_COMMIT_SHA" | grep -q "$RELEASE_BRANCH")"
+echo "git branch --contains "$CI_COMMIT_SHA" | grep -q "$DESIGNATED_BRANCH"": $(git branch --contains "$CI_COMMIT_SHA" | grep -q "$DESIGNATED_BRANCH")"
 
 # Check if CI_COMMIT_BEFORE_SHA is not all zeros
 if [[ "$CI_COMMIT_BEFORE_SHA" != "0000000000000000000000000000000000000000" ]]; then
