525a27f3fee058c9f66f180e10d62ae3382baeda
Aflal <aftocr@gmail.com>
2024-04-26 14:45:45 +0530

    Update validate_tag.sh

--- scripts/validate_tag.sh
+++ scripts/validate_tag.sh
@@ -11,10 +11,6 @@ if [[ -z $DESIGNATED_BRANCH ]]; then
     error_exit "Designated branch not provided."
 fi
 
-# Check if the release branch is provided
-if [[ -z $RELEASE_BRANCH ]]; then
-    error_exit "Release branch not provided."
-fi
 
 DESIGNATED_BRANCH_CHECK=false
 RELEASE_BRANCH_CHECK=false
@@ -37,12 +33,11 @@ check_designated_branch() {
     if [ $? -eq 0 ]; then
         echo "Within the designated branch"
         if [ $(git rev-parse "$DESIGNATED_BRANCH") == "$CI_COMMIT_SHA" ]; then
-            echo "Within the designated branch and latest commit"
-            RELEASE_BRANCH_CHECK=true
+            echo "Within the designated branch and latest commit" 
+            DESIGNATED_BRANCH_CHECK=true
         else
             error_exit "Within the designated branch but not the latest commit"
         fi
-        DESIGNATED_BRANCH_CHECK=true
     else
         echo "Not in the designated branch"
     fi
@@ -51,6 +46,11 @@ check_designated_branch() {
 # Check if the commit ID is in the release branch
 check_release_branch() {
 
+    tag_version=$(echo "$CI_COMMIT_TAG" | sed 's/v//')
+    # Extracting the minor version (e.g., for v1.2.5, it extracts 1.2)
+    minor_version=$(echo "$tag_version" | cut -d. -f1,2)
+    RELEASE_BRANCH=release/"$minor_version"
+    
     git fetch origin "$RELEASE_BRANCH" >/dev/null 2>&1 && git checkout "$RELEASE_BRANCH" >/dev/null 2>&1 && git branch --contains "$CI_COMMIT_SHA" >/dev/null 2>&1
 
 
@@ -60,9 +60,6 @@ check_release_branch() {
         # Extracting the version part after 'v' in the branch name
         branch_version=$(echo "$RELEASE_BRANCH" | sed 's/v//')
         # Extracting the version part after 'v' in the tag
-        tag_version=$(echo "$CI_COMMIT_TAG" | sed 's/v//')
-        # Extracting the minor version (e.g., for v1.2.5, it extracts 1.2)
-        minor_version=$(echo "$version" | cut -d. -f1,2)
         # Checking if the tag's minor version matches the provided minor version
         if [[ "$minor_version" == "$branch_version" ]]; then
             echo "minor versions match"
@@ -73,7 +70,7 @@ check_release_branch() {
                 error_exit "Within the release branch but not the latest commit"
             fi
         else
-            error_exit "Minor versions do not  match. branch: $RELEASE_BRANCH provided tag: $CI_COMMIT_TAG ideal tag format: "$branch_version".x "
+            error_exit "Minor versions do not  match. branch: release/"$minor_version" provided tag: $CI_COMMIT_TAG ideal tag format: "$minor_version".x "
         fi
     else
         echo "Not in the release branch"
