6e0ef0cab96fb1d5dba08f2e581ff4ef2784436b
Aflal <aftocr@gmail.com>
2024-05-03 09:36:29 +0530

    Update validate_tag.sh

--- scripts/validate_tag.sh
+++ scripts/validate_tag.sh
@@ -11,8 +11,6 @@ if [[ -z $DESIGNATED_BRANCH ]]; then
     error_exit "Designated branch not provided."
 fi
 
-
-
 tag_version=$(echo "$CI_COMMIT_TAG" | sed 's/v//')
 # Extracting the minor version (e.g., for v1.2.5, it extracts 1.2)
 minor_version=$(echo "$tag_version" | cut -d. -f1,2)
@@ -25,38 +23,6 @@ echo "Starting Tag Validation..."
 if [[ ! $CI_COMMIT_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     error_exit "Tag format is incorrect. Tags should follow the format v1.2.3"
 fi
-
-
-# if [[ $(git branch -a --contains tags/$minor_version) == *"$RELEASE_BRANCH"* ]]; then
-#     echo "Within the release branch"
-#     if [ $(git rev-parse "$RELEASE_BRANCH") == "$CI_COMMIT_SHA" ]; then
-#         echo "Tag: "$CI_COMMIT_TAG" is ithin the release branch: "$RELEASE_BRANCH" and latest commit: "$CI_COMMIT_SHA""
-#     else
-#         error_exit "Within the release branch: "$RELEASE_BRANCH" but not the latest commit"
-#     fi
-# else
-#     echo "Not in the release branch"
-#     if [ $(git rev-parse "$DESIGNATED_BRANCH") == "$CI_COMMIT_SHA" ]; then
-#         echo "Tag: "$CI_COMMIT_TAG" is ithin the release branch: "$DESIGNATED_BRANCH" and latest commit: "$CI_COMMIT_SHA""
-#     else
-#         error_exit "Commit is not in the designated branch: "$DESIGNATED_BRANCH" and Commit's branch name is not "$RELEASE_BRANCH""
-#     fi
-# fi
-
-
-#if [ $(git rev-parse "$DESIGNATED_BRANCH" 2>/dev/null) == "$CI_COMMIT_SHA" ]; then
-#    echo "Tag: "$CI_COMMIT_TAG" is within the designated branch: "$DESIGNATED_BRANCH" and latest commit: "$CI_COMMIT_SHA""
-#else
-#    if [[ $(git branch -a --contains tags/"$CI_COMMIT_TAG" 2>/dev/null) == *"$RELEASE_BRANCH"* ]]; then
-#        if [ $(git rev-parse "$RELEASE_BRANCH" 2>/dev/null) == "$CI_COMMIT_SHA" ]; then
-#             echo "Tag: "$CI_COMMIT_TAG" is ithin the release branch: "$RELEASE_BRANCH" and latest commit: "$CI_COMMIT_SHA""
-#        else
-#            error_exit "Within the release branch: "$RELEASE_BRANCH" but not the latest commit"
-#        fi
-#    else
-#        error_exit "Commit is not in the designated branch: "$DESIGNATED_BRANCH" and the branch based on is not "$RELEASE_BRANCH""
-#    fi
-#fi
 
 if  $(git checkout $RELEASE_BRANCH >/dev/null 2>&1) ; then
     if [[ $(git branch -a --contains tags/$CI_COMMIT_TAG 2>/dev/null) == *"$RELEASE_BRANCH"* ]]; then
